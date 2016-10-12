3800eaf0145dcb8bd9c859d8c5d13ac398f2abf2
Mujkic Ohran <ohran.mujkic@bfh.ch>
2016-10-12 18:07:26 +0200

    MODIFY ChartJS

--- src/ChartJS/README.md
+++ src/ChartJS/README.md
@@ -6,6 +6,7 @@
 
 ```sh
 
+<!-- Add ChartJS references -->
 <link rel="stylesheet"  href="lib/chartist/dist/chartist.min.css">
 <script src="lib/chartist/dist/chartist.min.js"></script>
 <script src="lib/ionic-i4mi/src/ChartJS/chartJS.js"></script>
@@ -15,38 +16,64 @@
 ## Usage
 
 ### Create a chart (Note this is an example!)
-1. To create a chart add in your template.html files (e.g chart.html)
+To create a chart add in your template.html files (e.g chart.html)
 
 ```sh
 
-<div class="ct-chartLine ct-perfect-fourth"></div>
-<div class="ct-chartBar ct-perfect-fourth"></div>
-<div class="ct-chartPie ct-perfect-fourth"></div>
+<div class="ct-chartExample ct-perfect-fourth"></div>
 
 ```
 
-2. Now let's create a chartJS object
-
+#### Line
+Add following lines in your controller to create a line chart
 
 ```sh
 
-.controller('ExampleCtrl', function($scope,I4MIMidataService) {
+var $configLine = {
+  name: '.ct-chartLine',
+  labels: 'Week',
+  series: "[12, 9, 7, 8, 5, 9, 0]",
+  fullWidth: "true",
+  showArea: "true",
+};
 
-configLine =
-  {
-    name: '.ct-chart',
-    labels: 'Week',
-    series: '[5, 3, 4]'
-  };
-
-var chartLine  = new ChartJS(configLine);
-var chartBar   = new ChartJS(configBar);
-var chartPie   = new ChartJS(configPie);
-
-// Draw chart
+var chartLine = new ChartJS($configLine);
 chartLine.line();
-chartBar.bar();
-chartPie.pie();
 
 })
+```
+$
+#### pie
+Add following lines in your controller to create a pie chart
+
+```sh
+
+var $configPie = {
+  name: '.ct-chartPie',
+};
+
+var data = {
+  series: [5, 3, 4]
+};
+var chartPie = new ChartJS($configPie);
+chartPie.pie(data);
+
+})
+```
+
+
+
+#### Bar
+Add following lines in your controller to create a bar chart
+
+```sh
+
+var $configBar = {
+  name: '.ct-chartBar',
+  labels: 'Year',
+  series: '[5, 4, 3, 7, 5, 10, 3, 4, 8, 10, 6, 8]',
+};
+var chartBar = new ChartJS($configBar);
+chartBar.bar();
+
 ```
--- src/ChartJS/chartJS.js
+++ src/ChartJS/chartJS.js
@@ -1,74 +1,85 @@
 var ChartJS = function(config)
 {
-  this.config = config;
+  $config = config;
 
 
   // Change value from weekly
 
-  if (config['labels'] = "Day"){
-      config['labels'] = ['2', '4', '6', '8', '10', '12', '14', '16', '18', '20', '22', '0'];
+  if ($config['labels'] == "Day"){
+      $config['labels'] = ['2', '4', '6', '8', '10', '12', '14', '16', '18', '20', '22'];
   }
 
 
-  if (config['labels'] = "Week"){
-      config['labels'] = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+  if ($config['labels'] == 'Week'){
+      $config['labels'] = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
   }
 
-  if (config['labels'] = "Month "){
-      config['month'] = 2;
-      config['year'] = 2016;
-      config['labels'] = "";
+  if ($config['labels'] == "Month "){
+      $config['month'] = 2;
+      $config['year'] = 2016;
+      $config['labels'] = "";
   }
 
-  if (config['labels'] = "Year"){
-      config['labels'] = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  if ($config['labels'] == "Year"){
+      $config['labels'] = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
   }
-
-
-
+  console.log($config['labels']);
 }
 
 ChartJS.prototype.line = function() {
-console.info("Create a ChartJS-Line-Object");
-
-var data = {
-  labels: config['labels'],
-  series: [config['series']]
-};
-
-new Chartist.Line(config['name'], data);
-
-
-};
-
-ChartJS.prototype.bar = function($config) {
-console.info("Create a ChartJS-Bar-Object");
-
-new Chartist.Bar(config['name'], {
-  labels: config['labels'],
-  series: [config['series']
-  ]
-}, {
-  axisX: {
-    position: 'start'
-  },
-  axisY: {
-    position: 'end'
-  }
-});
+  new Chartist.Line($config['name'], {
+    labels: $config['labels'],
+    series: [
+        [12, 9, 7, 8, 5, 9, 9],
+        [2, 1, 3.5, 7, 3, 9, 9],
+        [1, 3, 4, 5, 6, 9, 9]
+      ]
+    }, {
+    fullWidth: $config["fullWidth"],
+    low: 0,
+    showArea: $config["showArea"],
+    chartPadding: {
+      right: 40
+    }
+  });
 };
 
 
 
-ChartJS.prototype.pie = function($config) {
-console.info("Create a ChartJS-pie-Object");
-var data = {
-  series: config['series']
+ChartJS.prototype.bar = function() {
+  var data = {
+    labels: $config['labels'],
+    series: [
+      [5, 4, 3, 7, 5, 10, 3, 4, 8, 10, 6, 8]
+    ]
+  };
+
+  var options = {
+    seriesBarDistance: 10
+  };
+
+  var responsiveOptions = [
+    ['screen and (max-width: 640px)', {
+      seriesBarDistance: 5,
+      axisX: {
+        labelInterpolationFnc: function (value) {
+          return value[0];
+        }
+      }
+    }]
+  ];
+
+  new Chartist.Bar($config['name'], data, options, responsiveOptions);
 };
-var sum = function(a, b) { return a + b };
-new Chartist.Pie(config['name'], data, {
-  labelInterpolationFnc: function(value) {
-    return Math.round(value / data.series.reduce(sum) * 100) + '%';
-  }
-});
+
+
+
+ChartJS.prototype.pie = function(data) {
+  var sum = function(a, b) { return a + b };
+  new Chartist.Pie('.ct-chartPie', data, {
+    labelInterpolationFnc: function(value) {
+      return Math.round(value / data.series.reduce(sum) * 100) + '%';
+    }
+  });
+
 };
